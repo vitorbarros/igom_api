7a14b663d2dfd23c5bd89355cc3622d679fb3054
Vitor Barros <vitormonteirodebarros@gmail.com>
2018-04-24 15:38:22 -0300

    Ajustes

--- config/database.js
+++ config/database.js
@@ -7,7 +7,12 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 //for local development
-var mongoDB = mongoose.connect('mongodb://localhost/igom_db', {
+// var mongoDB = mongoose.connect('mongodb://localhost/igom_db', {
+//     useMongoClient: true
+// });
+
+//for production
+var mongoDB = mongoose.connect('mongodb://appAndroid:appAndroid@igomappcluster-shard-00-00-pwhah.mongodb.net:27017,igomappcluster-shard-00-01-pwhah.mongodb.net:27017,igomappcluster-shard-00-02-pwhah.mongodb.net:27017/igom_db?ssl=true&replicaSet=igomAppCluster-shard-0&authSource=admin', {
     useMongoClient: true
 });
 
